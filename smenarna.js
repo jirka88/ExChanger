ec5b85d9544bd499c2ec2b2289ad06f941ad0fb7
jirka88 <laval@centrum.cz>
2022-01-17 17:18:16 +0100

    fixing classes, error check

--- smenarna.js
+++ smenarna.js
@@ -1,15 +1,22 @@
 
 function vypocti() {
+    let tlacitko = document.getElementById("button");
     let vystup = document.getElementById("vystup");
     let castka = +document.getElementById("castka").value;
     let mena = document.querySelector(".section").value;
-    let NaMenu = document.querySelector(".section2").value;
+    let NaMenu = document.getElementsByClassName("section")[1].value;
 
     getText("https://freecurrencyapi.net/api/v2/latest?apikey=4363eb70-72b0-11ec-a578-918f3effcd01&base_currency=" + mena); //vznesení požadavku
 
     async function getText(file) {
-        let myObject = await fetch(file);
-        let myText = await myObject.json();
-        vystup.value = (parseFloat(myText.data[NaMenu]) * castka ).toFixed(3) + " " + NaMenu;
+        try {
+            let myObject = await fetch(file);
+            let myText = await myObject.json();
+            vystup.value = (parseFloat(myText.data[NaMenu]) * castka ).toFixed(3) + " " + NaMenu;
+            tlacitko.style.boxShadow = "6px 5px 0px green";
+        } catch (error) {
+            tlacitko.style.boxShadow = "6px 5px 0px red";
+            window.alert("Nastala chyba!")
+        }   
     }
 }
