d42d1173d32bec93fa1a8ac13ff0d45618d9d6c4
jirka88 <laval@centrum.cz>
2022-01-19 17:09:41 +0100

    better boxShadow click

--- smenarna.js
+++ smenarna.js
@@ -1,6 +1,11 @@
-
+var tlacitko = document.getElementById("button");
+    document.querySelectorAll(".section").forEach(item => { 
+        item.addEventListener("click", event => {
+        tlacitko.style.boxShadow = "6px 5px 0px black";
+    })
+  })
 function vypocti() {
-    let tlacitko = document.getElementById("button");
+  
     let vystup = document.getElementById("vystup");
     let castka = +document.getElementById("castka").value;
     let mena = document.querySelector(".section").value;
@@ -14,9 +19,10 @@ function vypocti() {
             let myText = await myObject.json();
             vystup.value = (parseFloat(myText.data[NaMenu]) * castka ).toFixed(3) + " " + NaMenu;
             tlacitko.style.boxShadow = "6px 5px 0px green";
-        } catch (error) {
+        } 
+        catch (error) {
             tlacitko.style.boxShadow = "6px 5px 0px red";
-            window.alert("Nastala chyba!")
+            window.alert("Nastala chyba!");
         }   
     }
 }
